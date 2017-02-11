11f209c89728ef19a0cfdc592b2f19b0424819a4
GunjaliSachdev <gunjali12smurfit@gmail.com>
2017-02-11 22:02:28 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,63 @@
 'use strict'
+
 const http = require('http')
+
 const Bot = require('messenger-bot')
 
+
+
+
 let bot = new Bot({
-  token: 'PAGE_TOKEN',
+
+  token: ' EAAIaV6dG3XEBACCzZA0wFARptoUQzLrZByo5wJ1ZAgC6TxQQhKP1jw4XInGq3p14BeJYBZAl1oZBNkT48P96i8PWbkVuIVxb814xreR4t5gaXD6ZAMDZB0seJ1JiWPdkrKZCLvmjX3QjZBjuYnqBV0WDlIPl1Wm3dQtHTx9kHlJjQWAZDZD',
+
   verify: 'VERIFY_TOKEN'
+
 })
+
+
+
 
 bot.on('error', (err) => {
+
   console.log(err.message)
+
 })
+
+
+
 
 bot.on('message', (payload, reply) => {
+
   let text = payload.message.text
 
+
+
+
   bot.getProfile(payload.sender.id, (err, profile) => {
+
     if (err) throw err
 
+
+
+
     reply({ text }, (err) => {
+
       if (err) throw err
 
+
+
+
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+
     })
+
   })
+
 })
 
+
+
+
 http.createServer(bot.middleware()).listen(3000)
+
